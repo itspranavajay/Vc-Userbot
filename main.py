cd3ee60c3d3fd02daf2444a98c61747150c314eb
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-02 02:54:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.types import Message
 from pytgcalls import PyTgCalls
 from pytgcalls_wrapper import Wrapper
 
 import youtube_dl
 from youtube_search import YoutubeSearch
 import requests
+import logging
+import time
+
+StartTime = time.time()
+logging.basicConfig(level=logging.INFO)
+
 
 API_ID = os.environ.get("API_ID", None) 
 API_HASH = os.environ.get("API_HASH", None) 
@@ -103,5 +110,34 @@ def song(client, message):
     except Exception as e:
         print(e)
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
+
+@app.on_message(filters.me & filters.command("ping", PREFIX))
+async def ping(_, message):
+    start_time = time.time()
+    m = await message.reply_text("Ping")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    uptime = get_readable_time((time.time() - StartTime))
+    await m.edit_text(f"Ping - `{ping_time}ms`\nUptime - {uptime}", parse_mode='markdown')
 
 pytgcalls.run()
